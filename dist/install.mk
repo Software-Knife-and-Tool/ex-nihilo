e1d8f7aa1a85588613a78b111b67320fb2879833
James M. Putnam <putnamjm.design@gmail.com>
2020-12-15 17:43:56 -0500

    #61 - dist

---
+++ dist/install.mk
@@ -0,0 +1,17 @@
+#
+# posix install makefile
+#
+ROOT = /opt
+BASE = gyre
+
+.PHONY: install uninstall help
+
+help:
+	@echo install - install gyre in $(ROOT)/$(BASE) (needs sudo)
+	@echo uninstall - remove gyre from $(ROOT)/$(BASE) (needs sudo)
+
+install:
+	@cat $(BASE)-0.0.1.tgz | (cd $(ROOT); tar xvfz -)
+
+uninstall:
+	@rm -rf $(ROOT)/$(BASE)
---
+++ dist/release.mk
@@ -0,0 +1,37 @@
+#
+# posix release makefile
+#
+BUILD = ../build
+GYRE = ../src/gyre/gyre
+CORE = ../src/gyre/core
+DEST = gyre
+LIBMU = ../src/libmu
+
+.PHONY: release clean help
+
+help:
+	@echo release - package release
+	@echo clean
+
+release:
+	@install -d $(DEST)
+	@install -d $(DEST)/bin
+	@install -d $(DEST)/include
+	@install -d $(DEST)/lib
+	@install -d $(DEST)/src
+	@install -d $(DEST)/src/gyre
+	@install -d $(DEST)/src/core
+	@install -m 755 ../dist/gyre.sh $(DEST)/bin/gyre
+	@install -m 755 ../dist/mu.sh $(DEST)/bin/mu
+	@install -m 755 $(BUILD)/mu-repl $(DEST)/bin
+	@install -m 755 $(BUILD)/mu-exec $(DEST)/bin
+	@install -m 644 $(BUILD)/libmu.a $(DEST)/lib
+	@install -m 644 $(BUILD)/libmu.so $(DEST)/lib
+	@install -m 644 $(GYRE)/*.l $(DEST)/src/gyre
+	@install -m 644 $(CORE)/*.l $(DEST)/src/core
+	@install -m 644 $(LIBMU)/libmu.h $(DEST)/include
+	@tar cvfz $(DEST)-0.0.1.tgz $(DEST)
+	@rm -rf $(DEST)
+
+clean:
+	@rm -rf $(DEST) $(DEST)-0.0.1.tgz
