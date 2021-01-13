8335f380a44b48c97abb13028c4a17db7ddfbf28
James M. Putnam <putnamjm.design@gmail.com>
2021-01-13 14:47:23 -0500

    #69 - intern namespace search broken

--- dist/gyre.sh
+++ dist/gyre.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 /usr/local/logica/bin/mu-exec                          \
     -l /usr/local/logica/canon/core/mu.l               \
-    -q '(:defcon lib-base "/usr/local/logica")'        \
-    -q '(load-once logica/canon "/canon/canon/lib.l")' \
-    -q '(in-ns (ns "user" (find-ns "canon")))'         \
+    -q '(:defsym lib-base "/opt/gyre/src")'            \
+    -q '(load-once gyre/canon "/core/gyre.l")'         \
+    -q '(in-ns (ns "user" (find-ns "core")))'          \
     "$@"                                               \
     -e '(repl)'
