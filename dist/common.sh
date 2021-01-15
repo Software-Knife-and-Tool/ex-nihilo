428754fc1436b98f141b9e1dec4999ff72ad9a30
James M. Putnam <putnamjm.design@gmail.com>
2021-01-15 11:37:15 -0500

    #69 - intern namespace search broken

--- dist/common.sh
+++ dist/common.sh
@@ -3,7 +3,6 @@
     -l /opt/gyre/src/core/mu.l                     \
     -l /opt/gyre/src/core/core.l                   \
     -q '(:defsym lib-base "/opt/gyre")'            \
-    -q '(require gyre/canon "/src/gyre/lib.l")'    \
-    -q '(in-ns (ns "user" (find-ns "gyre")))'      \
+    -q '(require common "/src/common/lib.l")'      \
     "$@"                                           \
-    -e '(repl)'
+    -i
