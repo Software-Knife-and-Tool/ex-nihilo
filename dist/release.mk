0ab749f8c546e21b4e505aad147f7fb9533e8f1b
putnamjm <putnamjm@sa.edu>
2021-01-19 22:19:17 -0500

    #116 - compiler design

--- dist/release.mk
+++ dist/release.mk
@@ -5,7 +5,7 @@ BUILD = ../build
 GYRE = ../src/gyre
 CORE = ../src/core
 COMMON = ../src/common
-COMPILER = ../src/compiler
+COMPILER = ../src/gyre/compiler
 DEST = gyre
 LIBMU = ../src/libmu
 
@@ -26,11 +26,11 @@ release:
 	@install -d $(DEST)/src/gyre
 	@install -d $(DEST)/src/core
 	@install -d $(DEST)/src/common
-	@install -d $(DEST)/src/compiler
-	@install -d $(DEST)/src/compiler/ast
-	@install -d $(DEST)/src/compiler/emit
-	@install -d $(DEST)/src/compiler/form
-	@install -d $(DEST)/src/compiler/opt
+	@install -d $(DEST)/src/gyre/compiler
+	@install -d $(DEST)/src/gyre/compiler/ast
+	@install -d $(DEST)/src/gyre/compiler/emit
+	@install -d $(DEST)/src/gyre/compiler/form
+	@install -d $(DEST)/src/gyre/compiler/opt
 	@install -m 755 ../dist/common.sh $(DEST)/bin/common
 	@install -m 755 ../dist/core.sh $(DEST)/bin/core
 	@install -m 755 ../dist/gyre.sh $(DEST)/bin/gyre
@@ -41,11 +41,11 @@ release:
 	@install -m 644 $(GYRE)/*.l $(DEST)/src/gyre
 	@install -m 644 $(CORE)/*.l $(DEST)/src/core
 	@install -m 644 $(COMMON)/*.l $(DEST)/src/common
-	@install -m 644 $(COMPILER)/*.l $(DEST)/src/compiler
-	@install -m 644 $(COMPILER)/ast/*.l $(DEST)/src/compiler/ast
-	@install -m 644 $(COMPILER)/emit/*.l $(DEST)/src/compiler/emit
-	@install -m 644 $(COMPILER)/form/*.l $(DEST)/src/compiler/form
-	@install -m 644 $(COMPILER)/opt/*.l $(DEST)/src/compiler/opt
+	@install -m 644 $(COMPILER)/*.l $(DEST)/src/gyre/compiler
+	@install -m 644 $(COMPILER)/ast/*.l $(DEST)/src/gyre/compiler/ast
+	@install -m 644 $(COMPILER)/emit/*.l $(DEST)/src/gyre/compiler/emit
+	@install -m 644 $(COMPILER)/form/*.l $(DEST)/src/gyre/compiler/form
+	@install -m 644 $(COMPILER)/opt/*.l $(DEST)/src/gyre/compiler/opt
 	@install -m 644 $(LIBMU)/libmu.h $(DEST)/include/libmu
 	@install -m 644 $(LIBMU)/platform/platform.h $(DEST)/include/libmu/platform
 	@tar cfz $(DEST)-0.0.1.tgz $(DEST)
